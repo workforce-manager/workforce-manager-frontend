f9e5675e8b76b2bf2ed2954d74d3884584727309
yuriivintoniak <you@example.com>
2025-05-02 16:57:15 +0300

    chore: configure tailwindcss with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
@@ -7,5 +8,6 @@ export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
+    tailwindcss(),
   ],
 });
