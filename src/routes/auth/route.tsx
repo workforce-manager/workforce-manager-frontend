a728c40a927f153117dce451aea2c6d796dd69af
yuriivintoniak <you@example.com>
2026-02-26 17:24:09 +0200

    style: update Auth footer typography and page background

--- src/routes/auth/route.tsx
+++ src/routes/auth/route.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/auth")({
   component: () => (
-    <div className="w-full h-screen flex items-center justify-center bg-[#2C2638]">
+    <div className="w-full h-screen flex items-center justify-center bg-[#3C364C]">
       <Outlet />
     </div>
   ),
